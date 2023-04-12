8f12d3401381a2bb134cc625cf8f415e1d4c8b8c
jakeperalta <Sushmina.Dhakal@triosstudent.com>
2023-04-12 09:23:31 -0400

    more changes

--- Question1.py
+++ Question1.py
@@ -1,15 +1,19 @@
 x= int(input("Enter number of years: "))
 l= 0
+#printing the number of year entered
 num= print("For year", x, ":")
 j= 0
 k=0
 total=0
 while(j<12):
     k= int(input(f"Enter the rainfall amount for {j+1}:"))
-    total= total+k
+    #using formula to calculate total
+    t= total+k
+    total= t/2.54
     j+=1
 print("For 12 months")
 print("Total rainfall:",total,"")
+#using total calculated to calculate average
 average = total/12
 print("Average monthly rainfall:",average )
 l=l+1
--- Question2.py
+++ Question2.py
@@ -1,4 +1,18 @@
 s= int(input("Starting number of organisms:"))
 p= int(input("Average daily percentage increase:"))
 m = int(input("Enter the number of days to display the final data:"))
-j=0
+
+if s<0:
+    print("error")
+elif p<0:
+    print("error")
+elif m<0:
+    print("error")
+else:
+     j=0
+     print("Day Approximate     Population")
+     while(j<m):
+        
+        print(f"{j+1}                 : {s}")
+        s=p/100*s
+        j+=1
