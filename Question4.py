9a76171780a71921f51f243154f46a2f85800be9
jakeperalta <Sushmina.Dhakal@triosstudent.com>
2023-04-12 10:43:39 -0400

    other changes made

--- Question4.py
+++ Question4.py
@@ -1,8 +1,10 @@
+#initialized a list
 myNumbers = [1,2,3,4,5,6,7,8,9,10]
 a=[]
 
 print("List of numbers:",myNumbers)
 
+#using for loop to print list of numbers greater than 5
 for n in myNumbers:
     if n> 5:
         a.append(n)
